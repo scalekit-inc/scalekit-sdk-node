d852f4cef5ce59abbde4fc54eb668c823e555c81
dhaneshbs <dhanesh@scalekit.com>
2025-06-02 22:11:54 +0530

    Update core.ts

--- src/core.ts
+++ src/core.ts
@@ -20,7 +20,7 @@ export default class CoreClient {
   public keys: JWK[] = [];
   public accessToken: string | null = null;
   public axios: Axios;
-  public sdkVersion = `Scalekit-Node/1.0.10`;
+  public sdkVersion = `Scalekit-Node/1.0.11`;
   public apiVersion = "20240430";
   public userAgent = `${this.sdkVersion} Node/${process.version} (${process.platform}; ${os.arch()})`;
   constructor(
