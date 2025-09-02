1c3d5f9645e76ba9fa6ba1ba31a4682d00c941ae
dhaneshbs <dhanesh@scalekit.com>
2025-09-02 19:03:19 +0530

    Release v2.1.2

--- lib/domain.d.ts
+++ lib/domain.d.ts
@@ -1,6 +1,7 @@
 import GrpcConnect from './connect';
 import CoreClient from './core';
-import { CreateDomainResponse, ListDomainResponse, DomainType } from './pkg/grpc/scalekit/v1/domains/domains_pb';
+import { CreateDomainResponse, GetDomainResponse, ListDomainResponse, DomainType } from './pkg/grpc/scalekit/v1/domains/domains_pb';
+import { Empty } from '@bufbuild/protobuf';
 export default class DomainClient {
     private readonly grpcConncet;
     private readonly coreClient;
@@ -11,16 +12,24 @@ export default class DomainClient {
      * @param {string} organizationId  The organization id
      * @param {string} name The domain name
      * @param {object} options The options to create a domain
-     * @param {DomainType} options.domainType The type of domain (ALLOWED_EMAIL_DOMAIN or ORGANIZATION_DOMAIN)
+     * @param {DomainType | string} options.domainType The type of domain (ALLOWED_EMAIL_DOMAIN or ORGANIZATION_DOMAIN)
      * @returns {Promise<CreateDomainResponse>} The created domain
     */
     createDomain(organizationId: string, name: string, options?: {
-        domainType?: DomainType;
+        domainType?: DomainType | string;
     }): Promise<CreateDomainResponse>;
+    /**
+     * Get a specific domain by ID for an organization
+     * @param organizationId The organization id
+     * @param domainId The domain id
+     * @returns {Promise<GetDomainResponse>} The domain details
+     */
+    getDomain(organizationId: string, domainId: string): Promise<GetDomainResponse>;
     /**
      * List domains for an organization
      * @param organizationId The organization id
      * @returns {Promise<ListDomainResponse>} The list of domains for the organization
      */
     listDomains(organizationId: string): Promise<ListDomainResponse>;
+    deleteDomain(organizationId: string, domainId: string): Promise<Empty>;
 }
