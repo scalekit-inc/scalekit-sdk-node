97988b33075bbe06e9cf6aaa1353cbf21ea76e6b
Hrishikesh Premkumar <Hrishikesh.premkumar@scalekit.com>
2026-03-10 15:18:24 +0530

    fix: skip bearer injection on token-endpoint calls
    The shared axios interceptor was adding Authorization: Bearer whenever
    accessToken was set, including on oauth/token requests (authenticateWithCode,
    generateClientToken, refreshToken). Token exchanges must be unauthenticated.
    
    Added skipAuth flag to InternalAxiosRequestConfig/AxiosRequestConfig and set
    skipAuth: true in CoreClient.authenticate() so the interceptor never injects
    a bearer header on token endpoint calls.

--- src/core.ts
+++ src/core.ts
@@ -1,5 +1,14 @@
 import { Code, ConnectError } from '@connectrpc/connect';
-import axios, { Axios, AxiosError, AxiosResponse, HttpStatusCode } from 'axios';
+import axios, { Axios, AxiosError, AxiosResponse, HttpStatusCode, InternalAxiosRequestConfig } from 'axios';
+
+declare module 'axios' {
+  interface InternalAxiosRequestConfig {
+    skipAuth?: boolean;
+  }
+  interface AxiosRequestConfig {
+    skipAuth?: boolean;
+  }
+}
 import { JWK } from 'jose';
 import os from 'os';
 import QueryString from 'qs';
@@ -39,7 +48,7 @@ export default class CoreClient {
       config.headers[headers['user-agent']] = this.userAgent;
       config.headers[headers['x-sdk-version']] = this.sdkVersion;
       config.headers[headers['x-api-version']] = this.apiVersion;
-      if (this.accessToken) {
+      if (this.accessToken && !config.skipAuth) {
         config.headers[headers.authorization] = `Bearer ${this.accessToken}`;
       }
 
@@ -70,6 +79,7 @@ export default class CoreClient {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
+      skipAuth: true,
     });
   }
 
