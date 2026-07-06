1da59fbe730bf81006354e233f5adf0ad993bc5a
Srinivas Karre <srinivas.karre@scalekit.com>
2026-07-06 15:43:29 +0530

    chore: bump apiVersion to 20260706 for release

--- src/core.ts
+++ src/core.ts
@@ -42,7 +42,7 @@ export default class CoreClient {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   public sdkVersion = `Scalekit-Node/${(require('../package.json') as { version: string }).version}`;
   // YYYYMMDD
-  public apiVersion = '20260612';
+  public apiVersion = '20260706';
   public userAgent = `${this.sdkVersion} Node/${process.version} (${
     process.platform
   }; ${os.arch()})`;
